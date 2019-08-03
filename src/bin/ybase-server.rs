c4c6296196df81cbded03f21ca81aed2111978b1
Zhenyun Yu <me@yuzhenyun.me>
2019-08-04 00:44:47 +0800

    ...

--- src/bin/ybase-server.rs
+++ src/bin/ybase-server.rs
@@ -7,14 +7,14 @@ use ybase::{
     error::Result,
 };
 
-use log::{info};
+use log::info;
 
 fn main() -> Result<()> {
     env_logger::from_env(env_logger::Env::default().default_filter_or("info")).init();
     info!("starting YBase now...");
     let meta_path = std::path::PathBuf::from("./meta.json");
-    let meta = FixMapStorageEngine::parse_meta(meta_path)?;
-    let storage_engine = Box::from(FixMapStorageEngine::new(meta));
+    let meta = FixMapStorageEngine::parse_meta(meta_path.clone())?;
+    let storage_engine = Box::from(FixMapStorageEngine::new(meta_path, meta));
     let engine = YBaseEngine::new(storage_engine);
     info!("init Ok, running...");
     engine.run();
--- src/storage/fixmap/engine.rs
+++ src/storage/fixmap/engine.rs
@@ -10,7 +10,7 @@ use uuid::Uuid;
 
 use serde::{Serialize, Deserialize};
 use serde_json;
-
+use log::info;
 use std::path;
 use crate::nexus::value::Value;
 use std::collections::HashMap;
@@ -21,7 +21,7 @@ use std::path::PathBuf;
 #[serde(deny_unknown_fields)]
 struct FieldDef {
     name: String,
-    value_type: String
+    value_type: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -40,26 +40,41 @@ pub struct Meta {
     pages: Vec<String>,
 }
 
+struct RuntimeContext {}
+
 pub struct FixMapStorageEngine {
+    meta_path: PathBuf,
     meta: Meta,
+    context: RuntimeContext,
 }
 
 impl FixMapStorageEngine {
-    pub fn new(meta: Meta) -> Self {
-        FixMapStorageEngine { meta }
+    pub fn new(meta_path: PathBuf, meta: Meta) -> Self {
+        FixMapStorageEngine { meta_path, meta, context: RuntimeContext {} }
     }
 
-    fn init(&mut self) {}
+    fn init(&mut self) {
+        info!("start init");
+        for page in self.meta.pages.clone().iter() {
+            let mut page_path = self.meta.root_dir.clone();
+            page_path.push(page);
+            self.mmap_file(&page_path);
+        }
+    }
 
     fn alloc_page(&mut self) -> Result<()> {
         let page_name = Uuid::new_v4().to_hyphenated().to_string() + ".page";
+        info!("allocating page with name: [{}]", &page_name);
         self.meta.pages.push(page_name.clone());
         let mut page_path = self.meta.root_dir.clone();
         page_path.push(page_name);
+        self.mmap_file(&page_path);
         Ok(())
     }
 
-    fn mmap_file(&mut self, file: &PathBuf) {}
+    fn mmap_file(&mut self, file: &PathBuf) {
+        info!("mmap file: [{:?}]", &file);
+    }
 
     pub fn parse_meta(meta_file: path::PathBuf) -> Result<Meta> {
         let content = std::fs::read_to_string(meta_file)?;
@@ -67,6 +82,10 @@ impl FixMapStorageEngine {
         // TODO: check fields legal
 
         Ok(meta)
+    }
+
+    fn sync_meta(&self) {
+        serde_json::to_string(&self.meta);
     }
 }
 
